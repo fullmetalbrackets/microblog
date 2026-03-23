e760d2de02ddea1b474f7528658601cf05f85a22
Ariel Diaz <fullmetalbrackets@gmail.com>
2026-03-22 23:04:31 -0400

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 export const SITE_TITLE = 'ariel.lol';
 export const SITE_DESCRIPTION = 'Things and stuff that come from my brain.';
 export const SITE_URL = 'https://ariel.lol/';
-export const MASTODON_URL = 'https://mastodon.social/@you';
-export const BLUESKY_URL = 'https://bsky.app/profile/you.bsky.social';
+export const MASTODON_URL = 'https://social.lol/@adiaz';
+export const BLUESKY_URL = 'https://bsky.app/profile/@fullmetalbrackets.com';
+export const GITHUB_URL = 'https://github.com/fullmetalbrackets';
